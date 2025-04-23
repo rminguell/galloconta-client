1c83cf58cd42e2f6a50074dfbb289ec0efd0a91c
Rodrigo Minguell <rodrigo.minguell@yahoo.com>
2025-04-23 13:39:26 +0200

    gtag

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,20 +1,20 @@
 import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
 export const metadata = {
   title: 'GalloConta',
   description: 'AI-powered crane counting app',
-}
+};
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
   display: 'swap',
-})
- 
+});
+
 export default async function LocaleLayout({
   children,
   params
@@ -22,15 +22,38 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: Promise<{locale: string}>;
 }) {
-
   const {locale} = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
- 
+
   return (
     <html lang={locale}>
+      <head>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TSJXQQKN');
+          `
+        }} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G4RSCNRHFT"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G4RSCNRHFT');
+          `
+        }} />
+      </head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSJXQQKN"
+                  height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
+        </noscript>
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
